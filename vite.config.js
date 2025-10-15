228690983c429462ae1c312ac62415f8710b9351
Xuan <xuan@MacBookPro.hitronhub.home>
2025-10-15 21:51:49 +0800

    fix: loadPath

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './example-react-web',
+  base: './',
   plugins: [react()],
   esbuild: {
     loader: 'jsx',
